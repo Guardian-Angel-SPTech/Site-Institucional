8c64f745b8cb066830557d35035d727f96a4dcca
LuisNunesH <luis.nunes@sptech.school>
2022-09-21 10:21:38 -0300

    Alterações nome de tabelas

--- database/GuardianAngel-database-v1.2.sql
+++ database/GuardianAngel-database-v1.2.sql
@@ -1,14 +1,14 @@
 create database GuardianAngel;
 use GuardianAngel;
 
-create table Empresa (
+create table empresa (
 idEmpresa int primary key auto_increment,
 cnpj char(14),
 email varchar(20),
 nomeEmpresa varchar(45)
 );
 
-create table Usuario (
+create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar(45),
 cpf char(11),
@@ -16,19 +16,19 @@ email varchar(30),
 senha varchar(45),
 nivelAcesso char(1),
 fkEmpresa int,
-foreign key Usuario(fkEmpresa) references Empresa(idEmpresa)
+foreign key usuario(fkEmpresa) references empresa(idEmpresa)
 );
 
-create table Maquina (
+create table maquina (
 idMaquina int primary key auto_increment,
 nomeMaquina varchar(45),
 MAC char(17),
 statusMaquina varchar(45),
 fkEmpresa int,
-foreign key Maquina(fkEmpresa) references Empresa(idEmpresa)
+foreign key maquina(fkEmpresa) references empresa(idEmpresa)
 );
 
-create table Chamado (
+create table chamado (
 idChamado int auto_increment,
 fkMaquina int,
 fkRegistro int,
@@ -37,7 +37,7 @@ hora datetime,
 primary key(idChamado, fkMaquina, fkRegistro)
 );
 
-create table Registro (
+create table registro (
 idRegistro int auto_increment,
 fkMaquina int,
 componente varchar(20),
@@ -46,19 +46,21 @@ horaRegistro datetime,
 primary key(idRegistro, fkMaquina)
 );
 
-create table Componente (
+create table componente (
 idComponente int primary key auto_increment,
 statusComponente varchar(10),
 nomeComponente varchar(20)
 );
 
-INSERT INTO Empresa (cnpj, email, nomeEmpresa)
+INSERT INTO empresa (cnpj, email, nomeEmpresa)
 	VALUES ('18406388000190', 'icefrezzy@gmail.com', 'Ice Frezzy');
     
-INSERT INTO Maquina (nomeMaquina, MAC, statusMaquina, fkEmpresa)
+INSERT INTO maquina (nomeMaquina, MAC, statusMaquina, fkEmpresa)
 	VALUES ('Maquina Operador 01', '72-7F-BA-D4-EC-D3', 'OK', 1);
     
-INSERT INTO Componente (statusComponente, nomeComponente)
+INSERT INTO componente (statusComponente, nomeComponente)
 	VALUES ('Ativo', 'Memória RAM'),
 			('Ativo', 'CPU'),
             ('Ativo', 'Disco');
+            
+select * from registro;
