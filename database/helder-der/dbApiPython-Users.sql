0805bc65736d54376cc1ea79afc42a445ed44c6b
RafaPiovezan <rafaella.filipe@Sptech.school>
2022-10-27 21:57:36 -0300

    Organizando repositórios

--- database/helder-der/dbApiPython-Users.sql
+++
@@ -1,8 +0,0 @@
-USE dbApiPython;
-
--- Usuário usado pelo sistema
-CREATE USER 'system'@'localhost' IDENTIFIED BY '!System_C0ntrol!';
-
-GRANT EXECUTE ON PROCEDURE stg_cadastrarMaquina TO 'system'@'localhost';
-
-FLUSH PRIVILEGES;
